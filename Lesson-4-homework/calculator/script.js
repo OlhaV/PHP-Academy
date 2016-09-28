b556ba26dd734d3b950ed878e12677d222e45cce
OlhaV <olga4irkova@gmail.com>
2016-09-28 22:30:42 +0300

    calculator fix

--- Lesson-4-homework/calculator/script.js
+++ Lesson-4-homework/calculator/script.js
@@ -2,30 +2,28 @@ var a = parseInt(prompt('Please enter the first number'));
 var b = +prompt('Please enter the second number');
 var operator = prompt('Please choose the operation: +, -, *, /')
 
-// first check the values of a and b 
+// check the values of a and b 
 if (!isNaN(a) && !isNaN(b)) {
-	// then check the operator 
-	if (operator == '+' || operator == '-' || 
-		operator == '*' || operator == '/') {
-		// if a, b and operator are correct, calculate
-		switch(operator) {
-			case '+': 
-				document.write(a + b);
-				break;
-			case '-': 
-				document.write(a - b);
-				break;
-			case '*':
-				document.write(a * b);
-				break;
-			case '/': 
-				document.write(a / b);
+	// check the operator 
+	switch(operator) {
+		case '+':
+			document.write(a + b);
+			// break;
+		case '-': 
+			document.write(a - b);
+			// break;
+		case '*':
+			document.write(a * b);
+			// break;
+		case '/': 
+			document.write(a / b);
+			// break;
+		// in case the operator is invalid
+		default: 
+			alert('Please select valid operator');
 		}
-	// in case the operator is invalid
-	} else {
-		alert('Please select valid operator');
 	} 
-// in case a and b are invalid 
-} else {
+	// in case a and b are invalid 
+else {
 	alert('Please enter valid numbers');
 }
