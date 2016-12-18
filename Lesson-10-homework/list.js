c7a93a6eba8a057761c9dcdbe7654932ccfb580c
olhav <olga4irkova@gmail.com>
2016-12-18 22:18:00 +0200

    Lesson 10 homework

--- Lesson-10-homework/list.js
+++ Lesson-10-homework/list.js
@@ -1,38 +1,36 @@
 ﻿function ListCtrl($scope) {
 
-    //$scope содержит данные модели. Это связующее звено между контроллером и видом.
-    //$scope всего лишь один из сервисов, внедренных в контроллер.
-    
-    // Мы создали модель с двумя делами в списке. Обратите внимание, что вашей модели нужно просто присвоить $scope
-    //и AngularJS отразит состояние в пользовательском интерфейсе самостоятельно. Модель данных это старый-добрый объект JavaScript, который не нужно чем либо оборачивать или изменять свойства через специальные сеттеры.
-
-    $scope.items = [
-      { text: 'Buy chocolate', done: false, date: "2016-12-12"},
-      { text: 'Pay bills', done: false, date: "2016-12-16"},
-      { text: 'Visit granpa', done: false, date: "2016-12-09"},
-      { text: 'Call mother', done: false, date: "2016-12-16"}
+    $scope.items = localStorage.getItem('entries') ? 
+    angular.fromJson(localStorage.getItem('entries')) : 
+    [
+      { text: 'Buy chocolate', done: false, date: "2016-12-12", priority: 'High'},
+      { text: 'Pay bills', done: false, date: "2016-12-16", priority: 'High'},
+      { text: 'Visit granpa', done: false, date: "2016-12-09", priority: 'Low'},
+      { text: 'Call mom', done: false, date: "2016-12-16", priority: 'Regular'}
     ];
- 
+
+    $scope.$watch('items', function() {
+    	localStorage.setItem('entries', angular.toJson($scope.items));
+    	console.log(angular.fromJson(localStorage['entries']));
+    }, true);
+
     $scope.addItem = function () {
         if($scope.itemText && $scope.itemDate) {
-            $scope.items.push({ text: $scope.itemText, done: false, date: $scope.itemDate});
-        } 
-        //Неизменяемый метод Array.push. Его вызов обновляет модель, которая затем обновляет вид посредством связывания данных.
-        //ng-repeat связан с этим массивом. Он автоматически проходится по массиву и добавляет новый DOM-элемент в вид. 
+        	var newEntry = {
+        		text: $scope.itemText, 
+        		done: false, 
+        		date: $scope.itemDate
+        	}; 
 
-        // $scope.itemText Из-за двунаправленного связывания данных модель всегда находится в актуальном состоянии. Это означает, что мы можем запросто читать состояние 
-        //пользовательского ввода. Нет необходимости регистрировать колбэки, обработчики событий или зависимости из API фреймворка.
-        $scope.itemText = '';
-        //Записать в текстовое поле пустой строки
+            $scope.items.push(newEntry);
+        }
+       $scope.itemText = '';
     };
  
 
     // Вычисляем количество оставшихся покупок.
     $scope.remain = function () {
         var count = 0;
-
-        // angular.forEach(...) - вызывает функцию iterator для каждого элемента коллекции obj, которая может быть объектом или массивом. Функция iterator вызывается как iterator(value, key), где value это значение текущего свойства объекта или элемент массива, и key это имя свойства в объекте или индекс элемента в массиве.
-
         angular.forEach($scope.items, function(item) {
             count += item.done;
         });
@@ -42,6 +40,12 @@
 
     $scope.removeItem = function() {
         $scope.items.splice(this.$index, 1);
-        // delete $scope.items[itemIndex];
+    }
+
+    $scope.changeItem = function() {
+    	var editedItem = prompt('Do you want to change this item?', $scope.items[this.$index].text);
+    	if(editedItem) {
+    		$scope.items[this.$index].text = editedItem;
+    	}
     }
 }
